a45cc5259ba550f280c81eb31b9fd96c97c88be8
DhruvRadadiya007 <dhruvradadiya101@gmail.com>
2026-09-09 12:05:29 +0530

    attributes and indexing_slicing

---
+++ array_indexing_slicing.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+print(" indexing ")
+
+arr = np.array([1,2,3,4,5,6])
+
+print(arr[0])
+print(arr[-1])
+
+print(" slicing ")
+
+print(arr[1:3])
+print(arr[:5])
+print(arr[::2])
+print(arr[::-1])
+
---
+++ attributes_arrays.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+print(" Array attributes ")
+print("\n")
+
+arr = np.array([[1,2,3,4,5],[6,7,8,9,0]])
+
+print(arr.shape) # number of rows and columns
+print(arr.ndim) # numer of dimnesions
+print(arr.size) # total number of elements 
+print(arr.dtype) # datatype
+print(arr.itemsize) # bytes per element
+
